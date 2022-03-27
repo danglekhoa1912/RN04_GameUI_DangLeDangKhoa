9779b6707e6d596f5f1726aae41c33643fa7239f
danglekhoa1912 <dangkhoa123bn@gmail.com>
2022-03-27 10:00:48 +0700

    finish Streaming screen

--- src/screens/SteamScreen/index.js
+++ src/screens/SteamScreen/index.js
@@ -18,7 +18,7 @@ class SteamScreen extends Component {
     return (
       <BackgroundView>
         <Text style={{marginLeft: 10}} header>
-          Sreaming
+          Streaming
         </Text>
         <View
           style={{
@@ -43,19 +43,75 @@ class SteamScreen extends Component {
         <View style={{marginHorizontal: 20}}>
           <Text subText>Populor Gamers</Text>
           <FlatList
-            style={{flexGrow: 0, marginVertical: 20}}
+            style={{flexGrow: 0, marginVertical: 15}}
             horizontal
             showsHorizontalScrollIndicator={false}
             ItemSeparatorComponent={() => <View style={{width: 30}} />}
             data={listGame}
             renderItem={({item}) => {
-              const {icon, preview} = item;
-              const backgroundImage = preview ? preview[0] : undefined;
+              const {icon} = item;
               return (
                 <Image
                   source={{uri: icon}}
                   style={{width: 80, height: 80, borderRadius: 10}}
                 />
+              );
+            }}
+          />
+        </View>
+        <View style={{marginHorizontal: 20}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: 15,
+            }}>
+            <Text title>Live Games</Text>
+            <TouchableOpacity>
+              <Text style={{color: COLORS.lightPurple}}>View all</Text>
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            contentContainerStyle={{paddingBottom: 80}}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => <View style={{height: 30}} />}
+            data={listGame}
+            renderItem={({item}) => {
+              const {preview, title} = item;
+              const backgroundImage = preview ? preview[0] : undefined;
+              return (
+                <View>
+                  <Image
+                    source={{uri: backgroundImage}}
+                    style={{width: '100%', height: 200, borderRadius: 8}}
+                  />
+                  <View
+                    style={{
+                      position: 'absolute',
+                      flexDirection: 'row',
+                      right: 0,
+                      padding: 5,
+                    }}>
+                    <Text
+                      style={{
+                        backgroundColor: COLORS.lightPurple,
+                        borderRadius: 5,
+                        marginRight: 5,
+                        padding: 3,
+                      }}>
+                      {title}
+                    </Text>
+                    <Text
+                      style={{
+                        backgroundColor: COLORS.lightRed,
+                        borderRadius: 5,
+                        padding: 3,
+                      }}>
+                      Live
+                    </Text>
+                  </View>
+                </View>
               );
             }}
           />
